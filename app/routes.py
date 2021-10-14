1a257600b27b0ecc4a4dd34709e78b277b215f2f
Brian <bstanton773@gmail.com>
2021-10-14 15:19:55 -0500

    Post update

--- app/routes.py
+++ app/routes.py
@@ -126,3 +126,25 @@ def my_posts():
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post_detail.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def post_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author.id != current_user.id:
+        flash('That is not your post. You may only edit posts you have created.', 'danger')
+        return redirect(url_for('my_posts'))
+    form = PostForm()
+    if form.validate_on_submit():
+        new_title = form.title.data
+        new_content = form.content.data
+        print(new_title, new_content)
+        post.title = new_title
+        post.content = new_content
+        db.session.commit()
+
+        flash(f'{post.title} has been saved', 'success')
+        return redirect(url_for('post_detail', post_id=post.id))
+
+    return render_template('post_update.html', post=post, form=form)
