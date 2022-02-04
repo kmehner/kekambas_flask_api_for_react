16d8af5d068a9af244863b65a4142b87a356132e
Brian <bstanton773@gmail.com>
2022-02-03 19:29:20 -0600

    Add requirements

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,10 @@
 from app import app
 from flask import render_template
-from app.blueprints.blog.models import Post
 
 
 @app.route('/')
 def index():
-    title = 'Coding Temple Flask'
-    posts = Post.query.all()
-    return render_template('index.html', title=title, posts=posts)
-
-
-@app.route('/products')
-def products():
-    title = 'Coding Temple Products'
-    products = ['apple', 'orange', 'banana', 'peach']
-    return render_template('products.html', title=title, products=products)
+    return render_template('index.html')
 
 
 
